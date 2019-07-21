736e4fe5ac5db0c93aaa53a05361e8123686091f
ljm <1123212@qq.com>
2019-07-21 17:50:00 +0800

    CMD-SeaJS

---
+++ 7.20（模块化）/05_CMD-SeaJS/src/index.js
@@ -0,0 +1,6 @@
+define(function (require) {
+  const m2 = require('./module2');
+
+  console.log(m2);
+  m2.showMsg();
+});
---
+++ 7.20（模块化）/05_CMD-SeaJS/src/module1.js
@@ -0,0 +1,11 @@
+define(function (require, exports, module) {
+  // (require, exports, module)，引入用的是commonJS
+  const name = 'rose';
+  const age = 29;
+
+  module.exports = {
+    name,
+    age
+  }
+
+});
