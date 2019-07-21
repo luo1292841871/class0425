020b30cd033eada38f8ad88c87669df91d34f2e7
ljm <1123212@qq.com>
2019-07-21 18:11:29 +0800

    CMD-SeaJS

--- 7.20（模块化）/01_CommonJS-Node/add.js
+++ 7.20（模块化）/01_CommonJS-Node/add.js
@@ -10,3 +10,15 @@ function mul(x, y) {
 // 外部要想使用模块内部的私有数据，需要暴露出去
 exports.add = add;
 exports.mul = mul;
+
+
+// 以下使用是错误的
+/*exports = {
+  add,
+  mul
+}*/
+
+/*module.exports = {
+  add,
+  mul
+}*/
